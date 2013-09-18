a081e40a88f848d94b88fbcd4755998eabd55395
QuillyT <quillyt@gmail.com>
2013-09-18 15:11:27 -0600

    UserInputTest: changed new test, using a read() instead.

--- lib/user_input.rb
+++ lib/user_input.rb
@@ -1,3 +1,14 @@
 class UserInput
-  
+  attr_accessor :command, :input_filename, :attribute, :criteria, :output_filename
+  def initialize(input = nil)
+    format(input)
+  end
+  def format(input)
+    if input
+      parts = input.split
+      @command = parts[0]
+    else
+      @command = ""
+    end
+  end
 end
--- test/user_input_test.rb
+++ test/user_input_test.rb
@@ -7,5 +7,25 @@ class UserInputTest < MiniTest::Test
     i = UserInput.new
     assert_kind_of UserInput,i
   end
+  def test_if_command_attribute_exists
+    i = UserInput.new
+    i.read('q')
+    assert_equal i.command,'q'
+  end
+  def test_if_command_is_set_to_help
+    i = UserInput.new
+  end
+  def test_if_command_is_set_to_queue_count
+  end
+  def test_if_command_is_set_to_queue_clear
+  end
+  def test_if_command_is_set_to_queue_print
+  end
+  def test_if_command_is_set_to_queue_print_by
+  end
+  def test_if_command_is_set_to_queue_save_to
+  end
+  def test_if_command_is_set_to_find
+  end
 end
 
