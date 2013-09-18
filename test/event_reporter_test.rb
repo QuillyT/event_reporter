61a02dc2b0882942e9c3f768777250993313249c
QuillyT <quillyt@gmail.com>
2013-09-17 19:23:31 -0600

    event_repoter test: added load file test - no argument should load default file

--- test/event_reporter_test.rb
+++ test/event_reporter_test.rb
@@ -9,10 +9,15 @@ class EventReporterTest < Minitest::Test
 		assert_kind_of EventReporter,er
 	end
   def test_load_file_test_gets_first_entry
-      er = EventReporter.new
-      er.load_file('event_attendees.csv')
-      a = er.attendee_list[0]
-      assert_equal 'Allison', a.first_name
+    er = EventReporter.new
+    er.load_file('event_attendees.csv')
+    a = er.attendee_list[0]
+    assert_equal 'Allison', a.first_name
   end
-
+  def test_load_file_when_there_is_no_filename
+    er = EventReporter.new
+    er.load_file()
+    a = er.attendee_list[0]
+    assert_equal 'Allison', a.first_name
+  end
 end
