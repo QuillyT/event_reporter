0934e96a048f475f2ddc95f29b92c1ac949e2591
QuillyT <quillyt@gmail.com>
2013-09-19 21:38:36 -0600

    saving. only event reporter updated so far.

--- lib/event_reporter.rb
+++ lib/event_reporter.rb
@@ -22,6 +22,7 @@ class EventReporter
 
   def process(input)
     entered_command,remainder = input.split(" ",2)
+    entered_command||=""
     found_command = commands.find{|command|command==entered_command.to_sym}
     execute(found_command,remainder)
   end
@@ -45,26 +46,59 @@ class EventReporter
   end
 
   def load_csv_file(filename)
+    binding.pry
     begin
       @contents = CSV.open filename, headers: true, header_converters: :symbol
+      OutputHandler.output(EventReporterMessage.loaded)
     rescue
-      puts "Error loading file."
+      OutputHandler.output(EventReporterMessage.load_error)
     end
   end
 
-  def find
-    OutputHandler.output("finding results")
+  def find(args)
+    args||=""
+    attribute,criteria = args.split(" ",2)
+    update_queue(attribute,criteria) if good_arguments?(attribute,criteria)
   end
 
-  def help
+  def update_queue(attribute,criteria)
+    results = @contents.find_all{|row|row[attribute.to_sym]==criteria}
+    push_attendees_to_queue(results)
+  end
+
+  def push_attendees_to_queue(results)
+    @queue = results.collect{|row|Attendee.new(row)}
+  end
+
+  def good_arguments?(attribute,criteria)
+    not (attribute_not_okay?(attribute)||criteria_nil?(criteria))
+  end
+
+  def attribute_not_okay?(attribute)
+    attribute_is_bad = not(valid_attributes.find_index attribute)
+    OutputHandler.output(EventReporterMessage.bad_attribute) if attribute_is_bad
+    attribute_is_bad
+  end
+
+  def criteria_nil?(criteria)
+    criteria_is_empty = criteria.nil?
+    OutputHandler.output(EventReporterMessage.criteria_empty) if criteria_is_empty
+    criteria_is_empty
+  end
+
+  def valid_attributes
+    ["first_name","last_name","phone_number","zipcode","city","state","address","email"]
+  end
+
+  def help(args=nil)
     OutputHandler.output("displaying help")
   end
 
-  def queue
+  def queue(args)
     OutputHandler.output("queue commands")
   end
 
-  def quit
+  def quit(args=nil)
     OutputHandler.output("Exiting Event Reporter. Goodbye!")
     exit true
   end
@@ -115,9 +149,8 @@ class EventReporter
 
   def initialize
     puts "Initializing Event Reporter!"
-    @content = []
+    @contents = []
     @queue = Queue.new
-    @help = Help.new
   end
 end
 
@@ -136,6 +169,18 @@ class EventReporterMessage
   end
   def self.invalid
     %(Invalid command.)
+  end
+  def self.loaded
+    %(Successfully loaded values from file.)
+  end
+  def self.load_error
+    %(Error loading file.)
+  end
+  def self.bad_attribute
+    %(Attribute for find is invalid. Type help for info.)
+  end
+  def self.criteria_empty
+    %(Critera for find is invalid. Type help for info.)
   end
   def self.load
   end
